312c188ecac24741038ef4dc8c138328a1a592b2
luna-nova <heartofnova@gmail.com>
2015-08-20 15:58:16 -0700

    multiple blocks with random moving

--- js/game.js
+++ js/game.js
@@ -1,9 +1,11 @@
 window.onload = function() {
     Crafty.init(800, 500);
-    // A blue block, controlled by arrow keys
-    var player = Crafty.e("2D, Canvas, Color, Keyboard, Collision, player")
+
+    // PLAYER
+
+    var player = Crafty.e("2D, Canvas, Keyboard, Collision, Image, player")
         .attr({x:225, y:375, w:50, h:50})
-        .color("blue")
+        .image("http://lparchive.org/Professor-Layton-and-the-Curious-Village/Update%2003/63-Simon.png")
         .bind('KeyDown', function(e) {
           if(e.key == Crafty.keys.LEFT_ARROW) {
             this.x -= 50;
@@ -23,11 +25,53 @@ window.onload = function() {
         }).collision().onHit("wall_bottom", function() {
           this.y -= 50;
         });
-    var enemyBlock = Crafty.e("2D, Canvas, Color, Collision, enemy_block")
-      .attr({x:75, y:75, w:50, h:50})
+
+    // ENEMIES
+
+    var enemyBlockA = Crafty.e("2D, Canvas, Color, Collision, enemy_block")
+      .attr({x:275, y:275, w:50, h:50})
       .color("red")
+      .bind("moving", randomMoving)
       .collision().onHit("player", function() {
         moveEnemy(this);
+      }).collision().onHit("wall_left", function() {
+        this.x += 100;
+      }).collision().onHit("wall_right", function() {
+        this.x -= 100;
+      }).collision().onHit("wall_top", function() {
+        this.y += 100;
+      }).collision().onHit("wall_bottom", function() {
+        this.y -= 100;
+      });
+    var enemyBlockB = Crafty.e("2D, Canvas, Color, Collision, enemy_block")
+      .attr({x:475, y:175, w:50, h:50})
+      .color("blue")
+      .bind("moving", randomMoving)
+      .collision().onHit("player", function() {
+        moveEnemy(this);
+      }).collision().onHit("wall_left", function() {
+        this.x += 100;
+      }).collision().onHit("wall_right", function() {
+        this.x -= 100;
+      }).collision().onHit("wall_top", function() {
+        this.y += 100;
+      }).collision().onHit("wall_bottom", function() {
+        this.y -= 100;
+      });
+    var enemyBlockC = Crafty.e("2D, Canvas, Color, Collision, enemy_block")
+      .attr({x:675, y:375, w:50, h:50})
+      .color("yellow")
+      .bind("moving", randomMoving)
+      .collision().onHit("player", function() {
+        moveEnemy(this);
+      }).collision().onHit("wall_left", function() {
+        this.x += 100;
+      }).collision().onHit("wall_right", function() {
+        this.x -= 100;
+      }).collision().onHit("wall_top", function() {
+        this.y += 100;
+      }).collision().onHit("wall_bottom", function() {
+        this.y -= 100;
       });
 
     // WALLS
@@ -51,4 +95,30 @@ window.onload = function() {
       enemy.x = ((Math.floor(Math.random() * 15)) * 50) + 25;
       enemy.y = ((Math.floor(Math.random() * 9)) * 50) + 25;
     }
+
+    function randomMoving () {
+      var direction = Math.floor(Math.random() * 4);
+      switch (direction) {
+        case 0:
+          this.x -= 50;
+          break;
+        case 1:
+          this.x += 50;
+          break;
+        case 2:
+          this.y -= 50;
+          break;
+        case 3:
+          this.y += 50;
+          break;
+      }
+    }
+
+    function ticker () {
+      Crafty.trigger("moving");
+    }
+
+    // GAMEPLAY
+
+    var movingTimer = setInterval(ticker, 250);
 };
